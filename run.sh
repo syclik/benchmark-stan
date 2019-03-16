77673d1d03627e80f7e0185f3d2439ed86442c7b
Daniel Lee <daniel@generable.com>
2019-03-16 16:50:47 -0400

    updating run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ echo "- [x] prepare the program to run the non-stiff integrator"
 echo "- [x] N/A. download the right versions of math"
 echo "- [x] build with math develop"
 echo "- [x] build with math feature/faster-ad-tls-v3"
-echo "- [ ] estimate order of magnitude to run a time"
-echo "- [ ] pick a number of times to run, N"
-echo "- [ ] pick a random seed"
+echo "- [x] estimate order of magnitude to run a time: 250 x 250 = ~6 min"
+echo "- [x] pick a number of times to run, 10"
+echo "- [x] pick a random seed, 3172019"
 echo "- [ ] repeat for number of threads 1, 2, 3, 4"
 echo "    - [ ] run and record develop 1 time with fixed seed"
 echo "    - [ ] run and record feature/faster-ad-tls-v3 with fixed seed"
@@ -24,11 +24,27 @@ echo "==========================================================================
 echo ""
 echo ""
 
-echo "Setup submodules"
+echo "Setup submodules" | tee progress.txt
 git submodule update --init --recursive
 
-echo "Building"
+echo "Building everything" | tee progress.txt
+echo "CPPFLAGS=-DSTAN_THREADS" > local
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "CXX=g++-5" >> local
+else
+    echo "CXX=clang++-6" >> local
+fi
 make build -j8
+
+random_seed=3172019
+N=10
+
+echo "Running once" | tee progress.txt
+
+echo "Compare output" | tee progress.txt
+tail -n +39 old-123-1.csv | sed -e :a -e '$d;N;2,5ba' -e 'P;D' > old-ref.csv
+tail -n +39 new-123-1.csv | sed -e :a -e '$d;N;2,5ba' -e 'P;D' > new-ref.csv
+
 
 
 
