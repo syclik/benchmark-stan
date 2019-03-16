1acd705256d56b25d41ebdc0a4b63382f31f6be5
Daniel Lee <daniel@generable.com>
2019-03-16 10:21:17 -0400

    updating run script

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo "TODO"
 echo "================================================================================"
 echo "- [x] prepare the program to run the non-stiff integrator"
 echo "- [x] N/A. download the right versions of math"
-echo "- [ ] build with math develop"
-echo "- [ ] build with math feature/faster-ad-tls-v3"
+echo "- [x] build with math develop"
+echo "- [x] build with math feature/faster-ad-tls-v3"
 echo "- [ ] estimate order of magnitude to run a time"
 echo "- [ ] pick a number of times to run, N"
 echo "- [ ] pick a random seed"
@@ -26,6 +26,11 @@ echo ""
 
 echo "Setup submodules"
 git submodule update --init --recursive
+
+echo "Building"
+make build -j8
+
+
 
 # if recording csv:
 # old/new, num threads, seed,  timing results
