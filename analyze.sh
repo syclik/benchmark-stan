116f14b9528de584551949d3d0f229628e73323d
Daniel Lee <daniel@generable.com>
2019-03-17 21:43:27 -0400

    updating analyze script

--- analyze.sh
+++ analyze.sh
@@ -3,15 +3,18 @@
 OS=(mac linux)
 N=10
 threads=(1 2 4)
+branch=(old new)
 
 
-echo "OS,type,minutes,seconds,threads" > time.txt
+echo "os,branch,type,minutes,seconds,threads" > time.txt
 for os in "${OS[@]}"; do
     for thread in "${threads[@]}"; do
 	echo "OS = $os;  thread = $thread"
-	grep "^real" $os/new-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,\1,\2,\3,$thread|" >> time.txt
-	grep "^sys" $os/new-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,\1,\2,\3,$thread|" >> time.txt
-	grep "^user" $os/new-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,\1,\2,\3,$thread|" >> time.txt
+	for b in "${branch[@]}"; do
+	    grep "^real" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
+	    grep "^sys" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
+	    grep "^user" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
+	done
     done
 done
 
