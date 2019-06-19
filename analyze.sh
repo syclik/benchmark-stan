fa5f76624dc8730fe80e4533c7e81d6bcbe14ef1
Daniel Lee <daniel@generable.com>
2019-06-19 09:21:02 -0400

    fixing script

--- analyze.sh
+++ analyze.sh
@@ -12,9 +12,9 @@ for os in "${OS[@]}"; do
 	echo "OS = $os;  thread = $thread"
 	for branch in "${branches[@]}"; do
 	    for model in "${models[@]}"; do
-		grep "^real" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
-		grep "^sys" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
-		grep "^user" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
+		grep "^real" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$branch,$model,\1,\2,\3,$thread|" >> time.txt
+		grep "^sys" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$branch,$model,\1,\2,\3,$thread|" >> time.txt
+		grep "^user" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$branch,$model,\1,\2,\3,$thread|" >> time.txt
 	    done
 	done
     done
