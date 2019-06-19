ff17e7d88273e550fcdd66ca83df0217f27fdd88
Daniel Lee <daniel@generable.com>
2019-06-19 09:10:46 -0400

    adding results for tls

--- analyze.sh
+++ analyze.sh
@@ -2,18 +2,20 @@
 
 OS=(mac linux)
 N=10
-threads=(1 2 4)
-branch=(old new)
+threads=(no-thread 1 2 4)
+branches=(old new)
+models=(schools-4 warfarin)
 
-
-echo "os,branch,type,minutes,seconds,threads" > time.txt
+echo "os,branch,model,type,minutes,seconds,threads" > time.txt
 for os in "${OS[@]}"; do
     for thread in "${threads[@]}"; do
 	echo "OS = $os;  thread = $thread"
-	for b in "${branch[@]}"; do
-	    grep "^real" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
-	    grep "^sys" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
-	    grep "^user" $os/$b-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,\1,\2,\3,$thread|" >> time.txt
+	for branch in "${branches[@]}"; do
+	    for model in "${models[@]}"; do
+		grep "^real" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
+		grep "^sys" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
+		grep "^user" $os/$branch-$model-$thread-time.txt | sed "s|\([a-z]*\).\([0-9]*\)m\([0-9.]*\)s|$os,$b,$model,\1,\2,\3,$thread|" >> time.txt
+	    done
 	done
     done
 done
